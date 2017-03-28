e29786ad16188a20068d334d27b05dc618bf4912
FaisalAli19 <faisalali1901@gmail.com>
2017-03-28 11:40:04 +0530

    created input field and created test for the same

---
+++ App/components/AddTodo.js
@@ -0,0 +1,29 @@
+const React = require('react');
+
+var AddTodo = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+
+        var newTodo = this.refs.todo.value;
+
+
+        if(newTodo.length > 0){
+            this.refs.todo.value = "";
+            this.props.onAddTodo(newTodo);
+        }else {
+            this.refs.todo.focus();
+        }
+    },
+    render: function() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="todos" ref="todo" placeholder="What you need to do?"/>
+                    <button type="submit" className="button expanded">Add Todo</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+module.exports = AddTodo;
